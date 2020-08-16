ee1cb3c66a488872af6902be29822362b14d0c0e
Jani Miettinen <n8396@student.jamk.fi>
2020-08-16 14:33:06 +0300

    test

--- src/main/java/projekti/PictureRepository.java
+++ src/main/java/projekti/PictureRepository.java
@@ -2,6 +2,9 @@ package projekti;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 public interface PictureRepository extends JpaRepository<Picture, Long> {
 
 }
